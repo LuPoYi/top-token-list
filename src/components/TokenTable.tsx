9abc9e38b8596a8588e86476c61d8cb2f97f8163
Bob <maydaybob2000@gmail.com>
2023-11-04 12:25:24 +0800

    feat: add image url to json

--- src/components/TokenTable.tsx
+++ src/components/TokenTable.tsx
@@ -1,7 +1,18 @@
+"use client"
+
 import React from "react"
+import {
+  avalanche,
+  bsc,
+  mainnet,
+  polygon,
+  arbitrum,
+  optimism,
+} from "@wagmi/core/chains"
 
 interface Token {
   chainId: number
+  image: string
   address: string
   decimals: number
   symbol: string
@@ -12,40 +23,73 @@ interface TokenTableProps {
   tokenList: Token[]
 }
 
+const expolorUrlMap: Record<number, string> = {
+  [mainnet.id]: mainnet.blockExplorers.default.url,
+  [polygon.id]: polygon.blockExplorers.default.url,
+  [arbitrum.id]: arbitrum.blockExplorers.default.url,
+}
 export const TokenTable = ({ tokenList }: TokenTableProps) => {
+  const addTokenToMM = async (token: Token) => {
+    try {
+      const { ethereum } = window as any
+      await ethereum.request({
+        method: "wallet_watchAsset",
+        params: {
+          type: "ERC20",
+          options: token,
+        },
+      })
+    } catch (ex) {
+      console.error(ex)
+    }
+  }
+
   return (
     <div className="overflow-x-auto">
       <table className="table">
-        {/* head */}
         <thead>
           <tr>
             <th>Symbol & Name</th>
-
             <th>Address</th>
             <th></th>
           </tr>
         </thead>
         <tbody>
-          {tokenList.map(({ symbol, address, decimals, name }) => (
-            <tr key={address}>
-              <td>
-                {symbol}
-                <br />
-                <span className="badge badge-ghost badge-sm">{name}</span>
-              </td>
-              <td>{address}</td>
-              <th></th>
-            </tr>
-          ))}
+          {tokenList.map((token) => {
+            const { chainId, symbol, address, name, image } = token
+            return (
+              <tr key={address}>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-8 h-8">
+                        <img src={image} alt={`${symbol} ${name}`} />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{symbol}</div>
+                      <div className="text-sm opacity-50">{name}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <a
+                    href={`${expolorUrlMap[chainId]}/token/${address}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {address}
+                  </a>
+                </td>
+                <th>
+                  <button onClick={() => addTokenToMM(token)}>
+                    Add {symbol} to MetaMask
+                  </button>
+                </th>
+              </tr>
+            )
+          })}
         </tbody>
-        {/* foot */}
-        <tfoot>
-          <tr>
-            <th>Symbol & Name</th>
-            <th>Address</th>
-            <th></th>
-          </tr>
-        </tfoot>
       </table>
     </div>
   )
